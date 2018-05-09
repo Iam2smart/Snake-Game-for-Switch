0501a769ef33d021da6c88ece9f2c2b715dae78b
Iam2smart <calebtrobins@gmail.com>
2018-05-09 13:20:18 -0600

    Update Snake.c

--- Snake.c
+++ Snake.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-#include "draw.h"
+#include "program.h"
 #include "Snake.h"
+#include "input.h"
+
+/**
+This is The Code that does mostly everything for the Snake Game.
+**/
+
+#define xMinBoundry 0
+#define xMaxBoundry 427
+#define yMinBoundry 0
+#define yMaxBoundry 240
+
+
 
